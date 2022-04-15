9ea072157299ed306a7f07701128999b832765ea
Daveran <100153676+Daveran@users.noreply.github.com>
2022-04-15 14:40:57 +0200

    Igen
    Destroyable Platform és Moving Platform prefabs

---
+++ AngelaForce/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public int startingPoint;
+    public Transform[] points;
+    public bool sticky = false;
+    private int i;
+    private void Start()
+    {
+        transform.position = points[startingPoint].position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f) {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        
+        }
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(sticky.Equals(true))
+            collision.transform.SetParent(transform);
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (sticky.Equals(true))
+            collision.transform.SetParent(null);
+    }
+}
